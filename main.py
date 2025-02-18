b268e4067430a75793775bb6585ae6928f5ff515
xjirada <aldrichsurya@gmail.com>
2025-02-18 16:37:26 +0700

    penambahan function terbaru yaitu fucntion pembagian di dalam codingan

--- main.py
+++ main.py
@@ -10,9 +10,13 @@ def pengurangan(a,b):
 	totalPengurangan = a-b
 	return (totalPengurangan)
 
+def pembagian(a,b):
+        total=a/b
+        return(total)
 def main():
 	print (penambahan(5,10))
 	print (perkalian(6,10))
 	print (pengurangan(10,5))
+    print (pembagian(12919201,293902392))
 
 main()
